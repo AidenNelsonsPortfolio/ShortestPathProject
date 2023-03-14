b4e97b32e078297f6d419c340b0badc22e6e5ec7
Aiden <aidenjnelson1@gmail.com>
2023-03-14 13:13:12 -0500

    Initial project setup, test files, rubric.

---
+++ shortestPathProject.py
@@ -0,0 +1,8 @@
+# Author: Aiden Nelson
+# Date Created: 3/14/2023
+# Date Last Modified: 3/14/2023
+
+# Description: This program will find the shortest path between nodes in different types of graphs
+# using algorthims such as Dijkstra's, Bellman-Ford, and DAG shortest path.
+
+
